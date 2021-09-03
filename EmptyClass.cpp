97b881e45e5151015a1539f526f30556efd076d9
bsvijayan <bsvijayan@gmail.com>
2021-09-03 14:02:23 +0530

    Create EmptyClass.cpp

---
+++ EmptyClass.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+class base
+{
+    
+};
+int main()
+{
+    cout<<"Size of Empty class : "<<sizeof(base);
+    
+
+    return 0;
+}
+//output
+Size of Empty class : 1
