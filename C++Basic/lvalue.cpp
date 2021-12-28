4321a4ccbe01d774528cf10e8f75122eb5b84f0b
bsvijayan <bsvijayan@gmail.com>
2021-12-28 12:20:52 +0530

    Lvalue.cpp

--- C++Basic/lvalue.cpp
+++ C++Basic/lvalue.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
+//Rvalues
+//int x=100;
+//100 is an Rvalue - short lived.it's literal constant and no specific memory address
+//rvalues is contained in the containers.
+
+//Lvalues -live longer life and since exist as variables
+//Lvalues - as containers 
+//L Value - Variable and have address
+//C++ Assignement needs Lvalue as left operand
 
 int main()
 {
    int i;
    100=i;
-   
+
     return 0;
 }
-
-//output
-//main.cpp:14:8: error: lvalue required as left operand of assignment
- //  14 |    100=i;
-  //    |        ^
