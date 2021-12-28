2a576ba88def2c6b8e03970388ad12157129759e
bsvijayan <bsvijayan@gmail.com>
2021-12-28 12:12:44 +0530

    lValue.cpp

---
+++ C++Basic/lvalue.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int i;
+   100=i;
+   
+    return 0;
+}
+
+//output
+//main.cpp:14:8: error: lvalue required as left operand of assignment
+ //  14 |    100=i;
+  //    |        ^
